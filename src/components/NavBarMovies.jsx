1f226a490ac40795d445f1ed323a468c7a522bb8
marreropd <pablomarrerofernandez1@gmail.com>
2022-04-18 14:34:31 -0300

    update

--- src/components/NavBarMovies.jsx
+++ src/components/NavBarMovies.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Dropdown, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function NavbarMovies() {
@@ -16,47 +16,40 @@ function NavbarMovies() {
           <Navbar.Collapse id="navbar-dark-example">
             <div className="d-flex align-items-center">
               <Link
-                className="text-decoration-none ms-2  text-white nav-links"
-                to="/"
+                className="text-decoration-none ms-2 mt-1  text-white nav-links"
+                to="/about"
               >
                 About
               </Link>
-
+            </div>
+            <div className="d-flex align-items-center">
               <Link
-                className="text-decoration-none ms-2 text-white nav-links"
+                className="text-decoration-none text-secondary ms-2 mt-1 text-white nav-links"
                 to="/"
               >
                 Contact
               </Link>
             </div>
-            <Nav className=" sticky-top fixed-top">
-              <div className="d-flex justify-content-end">
-                <div>
-                  <NavDropdown
-                    className="ms-2"
-                    id="nav-dropdown-dark-example"
-                    title="Filter"
-                    menuVariant="dark"
+
+            <Nav className="ms-auto sticky-top me-3">
+              <NavDropdown className="ms-2 " title="Filter" menuVariant="dark">
+                <NavDropdown.Item href="#action/3.1">
+                  <Link
+                    className="text-decoration-none text-white"
+                    to="/filter/title"
                   >
-                    <NavDropdown.Item href="#action/3.1">
-                      <Link
-                        className="text-decoration-none text-white"
-                        to="/filter/title"
-                      >
-                        Filtrar by title
-                      </Link>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                      <Link
-                        className="text-decoration-none text-white"
-                        to="/filter/rating"
-                      >
-                        Filter by rating
-                      </Link>
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </div>
-              </div>
+                    Filtrar by title
+                  </Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  <Link
+                    className="text-decoration-none text-white"
+                    to="/filter/rating"
+                  >
+                    Filter by rating
+                  </Link>
+                </NavDropdown.Item>
+              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
